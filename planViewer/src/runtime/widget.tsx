01687ca74c764bd65d49da7c597845855068250b
ddattilio20 <ddattilio20@gmail.com>
2025-05-29 08:15:57 -0500

    Querying feature layer to get features is now working

--- planViewer/src/runtime/widget.tsx
+++ planViewer/src/runtime/widget.tsx
@@ -1,11 +1,37 @@
 import { React, type AllWidgetProps } from 'jimu-core'
 import type { IMConfig } from '../config'
+import Query from "esri/rest/support/Query"
+import FeatureLayer from 'esri/layers/FeatureLayer';
+import { ResultsFieldSetting } from 'dist/widgets/arcgis/query/src/setting/results-field';
+import { useEffect, useState } from 'react';
 
 const Widget = (props: AllWidgetProps<IMConfig>) => {
+
+const [county, setCounty] = useState(null);
+const [em, setEM] = useState(null)
+
+
+let layer = new FeatureLayer({
+      portalItem: {
+        id: "5664ec66e2e14022accea039773a2712"
+      }
+    })
+
+   useEffect(() => {
+      layer.queryFeatures().then(results => {
+            setCounty(results.features[2].attributes.name_of_county)
+            setEM(results.features[2].attributes.emergency_manager)
+      })
+        
+      
+   }, [])
+
+
+
+
   return (
     <div className="widget-demo jimu-widget m-2">
-      <p>Simple Widget</p>
-      <p>exampleConfigProperty: {props.config.exampleConfigProperty}</p>
+      <h1>EOP Plan for {county} County. Prepared by {county} county Emergency Manager {em}.</h1>
     </div>
   )
 }
