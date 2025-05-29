4510a02f19e6ca5d7306e91295ee32bf409d702a
ddattilio20 <ddattilio20@gmail.com>
2025-05-29 14:01:20 -0500

    Commits before weekend

--- planViewer/src/runtime/widget.tsx
+++ planViewer/src/runtime/widget.tsx
@@ -8,7 +8,8 @@ import { useEffect, useState } from 'react';
 const Widget = (props: AllWidgetProps<IMConfig>) => {
 
 const [county, setCounty] = useState(null);
-const [em, setEM] = useState(null)
+const [em, setEM] = useState(null);
+const [longResponse1, setLongResponse1] = useState(null);
 
 
 let layer = new FeatureLayer({
@@ -21,6 +22,7 @@ let layer = new FeatureLayer({
       layer.queryFeatures().then(results => {
             setCounty(results.features[2].attributes.name_of_county)
             setEM(results.features[2].attributes.emergency_manager)
+            setLongResponse1(results.features[2].attributes.animal_sheltering_question_long)
       })
         
       
@@ -31,7 +33,10 @@ let layer = new FeatureLayer({
 
   return (
     <div className="widget-demo jimu-widget m-2">
-      <h1>EOP Plan for {county} County. Prepared by {county} county Emergency Manager {em}.</h1>
+      <h1>LEOP Plan for {county} County. Prepared by {county} county Emergency Manager {em}.</h1><br></br>
+      <p>This would be a template text paragraph, pretend it is much larger. This would be a template text paragraph, pretend it is much larger. 
+        This would be a template text paragraph, pretend it is much larger. Approach to animal sheltering</p>
+      <p>{longResponse1}</p>
     </div>
   )
 }
