e930b1be23b43e0316105b1f413b7f63762ef34f
ddattilio20 <ddattilio20@gmail.com>
2025-07-12 16:46:48 -0500

    Test plan viewer setup

--- planViewer/src/runtime/widget.tsx
+++ planViewer/src/runtime/widget.tsx
@@ -8,21 +8,24 @@ import { useEffect, useState } from 'react';
 const Widget = (props: AllWidgetProps<IMConfig>) => {
 
 const [county, setCounty] = useState(null);
-const [em, setEM] = useState(null);
-const [longResponse1, setLongResponse1] = useState(null);
+const [planName, setPlanName] = useState(null);
+const [template1, setTemplate1] = useState(null);
+const [placeholder1, setPlaceholder1] = useState(null);
 
 
 let layer = new FeatureLayer({
       portalItem: {
-        id: "5664ec66e2e14022accea039773a2712"
+        id: "fe99b3f489534897838074a87ed7e425"
       }
     })
 
    useEffect(() => {
       layer.queryFeatures().then(results => {
-            setCounty(results.features[2].attributes.name_of_county)
-            setEM(results.features[2].attributes.emergency_manager)
-            setLongResponse1(results.features[2].attributes.animal_sheltering_question_long)
+            setCounty(results.features[0].attributes.county_name)
+            setPlanName(results.features[0].attributes.plan_name)
+            setTemplate1(results.features[0].attributes.template_1)
+            setPlaceholder1(results.features[0].attributes.placeholder_1)
+           
       })
         
       
@@ -33,10 +36,9 @@ let layer = new FeatureLayer({
 
   return (
     <div className="widget-demo jimu-widget m-2">
-      <h1>LEOP Plan for {county} County. Prepared by {county} county Emergency Manager {em}.</h1><br></br>
-      <p>This would be a template text paragraph, pretend it is much larger. This would be a template text paragraph, pretend it is much larger. 
-        This would be a template text paragraph, pretend it is much larger. Approach to animal sheltering</p>
-      <p>{longResponse1}</p>
+      <h1>This is the {planName} prepared by {county} County Emergency Management.</h1>
+      <p>{template1}</p>
+      <p>{placeholder1}</p>
     </div>
   )
 }
